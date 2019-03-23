667fafab79faaad1db650f6df59a6e2a50ea2b02
Travis Williamson <cthwgw1791@gmail.com>
2019-03-22 22:46:40 -0700

    Finally got the timezone calculation to work in real time with bus arrival time and minutes away. Also fixed the styling to avoid the jumble of text for the input.

--- assets/logic.js
+++ assets/logic.js
@@ -14,7 +14,7 @@
   
   * Users from many different machines must be able to view same train times.
   
-//  */ 
+//  */
 
 //create a function to dynamically display local KST
 const dateTime = $('#datetime');
@@ -25,14 +25,12 @@ moment.tz.add("Asia/Seoul|LMT KST JST KST KDT KDT|-8r.Q -8u -90 -90 -9u -a0|0123
 // function to render clock
 setInterval(() => {
   // Adding timezone KST to moment and formatting
-  const kst = moment().tz('Asia/Seoul').format("DD  MMM  HH:mm:ss");
+  const kst = moment().tz('Asia/Seoul').format("DD MMM  HH:mm:ss");
   dateTime.text(`KST: ${kst}`);
 }, 1000);
 
-
-
 // Initialize Firebase
-var config = {
+const config = {
   apiKey: "AIzaSyCU9Dz4cZg_dGXsPpoHMnj8cNL6C9YQMv0",
   authDomain: "spaceport-time-table.firebaseapp.com",
   databaseURL: "https://spaceport-time-table.firebaseio.com",
@@ -42,20 +40,20 @@ var config = {
 };
 firebase.initializeApp(config);
 
-var database = firebase.database();
+const database = firebase.database();
 
 // 2. Button for adding buses
 $("#add-bus-btn").on("click", function (event) {
   event.preventDefault();
 
   // Grabs user input
-  var  busNum= $("#bus-number-input").val().trim();
-  var destination = $("#destination-input").val().trim();
-  var firstBus = moment($("#first-bus-input").val().trim(), "HH:mm").format("X");
-  var frequency = $("#frequency-input").val().trim();
-  
+  const busNum = $("#bus-number-input").val().trim();
+  const destination = $("#destination-input").val().trim();
+  const firstBus = $("#first-bus-input").val().trim();
+  const frequency = $("#frequency-input").val().trim();
+
   // Creates local "temporary" object for holding bus info
-  var newBus = {
+  const newBus = {
     bus: busNum,
     dest: destination,
     start: firstBus,
@@ -65,67 +63,59 @@ $("#add-bus-btn").on("click", function (event) {
   // Uploads employee data to the database
   database.ref().push(newBus);
 
-  // Logs everything to console
-  console.log(newBus.bus);
-  console.log(newBus.destination);
-  console.log(newBus.firstBus);
-  console.log(newBus.frequency);
-
-  alert("Bus successfully added");
-
   // Clears all of the text-boxes
-  $("#employee-name-input").val("");
-  $("#role-input").val("");
-  $("#start-input").val("");
-  $("#rate-input").val("");
+  $("#bus-number-input").val("");
+  $("#destination-input").val("");
+  $("#first-bus-input").val("");
+  $("#frequency-input").val("");
 });
 
-// 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
+// 3. Create Firebase event for adding bus to the database and a row in the html when a user adds an entry
 database.ref().on("child_added", function (childSnapshot) {
   console.log(childSnapshot.val());
 
-  // Store everything into a variable.
-  var empName = childSnapshot.val().name;
-  var empRole = childSnapshot.val().role;
-  var empStart = childSnapshot.val().start;
-  var empRate = childSnapshot.val().rate;
+  // Store everything into a constiable.
+  const busNum = childSnapshot.val().bus;
+  const destination = childSnapshot.val().dest;
+  const firstBus = childSnapshot.val().start;
+  const frequency = childSnapshot.val().rate;
 
-  // Employee Info
-  console.log(empName);
-  console.log(empRole);
-  console.log(empStart);
-  console.log(empRate);
 
-  // Prettify the employee start
-  var empStartPretty = moment.unix(empStart).format("MM/DD/YYYY");
 
-  // Calculate the months worked using hardcore math
-  // To calculate the months worked
-  var empMonths = moment().diff(moment(empStart, "X"), "months");
-  console.log(empMonths);
+  //Current time KST
+  const kST = moment().tz('Asia/Seoul').format("HH:mm");
+  console.log(kST);
 
-  // Calculate the total billed rate
-  var empBilled = empMonths * empRate;
-  console.log(empBilled);
+  // First Time (pushed back 1 year to make sure it comes before current time)
+  const firstTimeConverted = moment.tz(firstBus, "HH:mm", 'Asia/Seoul').subtract(1, "years");
+  
+
+  // Difference between the times
+  var diffTime = moment.tz('Asia/Seoul').diff(moment(firstTimeConverted), "minutes");
+ 
+
+// Time apart (remainder)
+var tRemainder = diffTime % frequency;
+
+
+// Minutes til next bus
+var tMinutesTillBus = frequency - tRemainder;
+
+
+// Next bus arrival time
+var nextBus = moment.tz('Asia/Seoul').add(tMinutesTillBus, "minutes");
+
 
   // Create the new row
-  var newRow = $("<tr>").append(
-    $("<td>").text(empName),
-    $("<td>").text(empRole),
-    $("<td>").text(empStartPretty),
-    $("<td>").text(empMonths),
-    $("<td>").text(empRate),
-    $("<td>").text(empBilled)
+  const newRow = $("<tr>").append(
+    $("<td>").text(busNum),
+    $("<td>").text(destination),
+    $("<td>").text(firstBus),
+    $("<td>").text(frequency),
+    $("<td>").text(moment(nextBus).format("HH:mm")),
+    $("<td>").text(tMinutesTillBus)
   );
 
   // Append the new row to the table
-  $("#employee-table > tbody").append(newRow);
+  $("#bus-table > tbody").append(newRow);
 });
-
-  // Example Time Math
-  // -----------------------------------------------------------------------------
-  // Assume Employee start date of January 1, 2015
-  // Assume current date is March 1, 2016
-
-  // We know that this is 15 months.
-  // Now we will create code in moment.js to confirm that any attempt we use meets this test case
